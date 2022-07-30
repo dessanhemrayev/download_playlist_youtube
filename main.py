8ba7295dc30e03eb51c77994e0f0a07541ca0c49
dessanhemrayev <43703214+dessanhemrayev@users.noreply.github.com>
2022-07-30 16:28:58 +0000

    переделан метод скачки playlist-a из youtube

---
+++ main.py
@@ -0,0 +1,25 @@
+import re
+from pytube import Playlist
+
+def youtube_playlists_download(main_directory,playlists):
+    for playlist in playlists:
+        playlist = Playlist(playlist)   
+        DOWNLOAD_DIR = main_directory + '/youtube/'+playlist.title
+        playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")    
+        print(len(playlist.video_urls))    
+        for url in playlist.video_urls:
+            print(f"****{url.title} **** {url} ***")    
+        for video in playlist.videos:
+            print('downloading : {} with url : {}'.format(video.title, video.watch_url))
+            video.streams.\
+                filter(type='video', progressive=True, file_extension='mp4').\
+                order_by('resolution').\
+                desc().\
+                first().\
+                download(DOWNLOAD_DIR)
+    return 'Good lik'
+
+if __name__ == "__main__":
+    youtube_playlists=['https://youtube.com/playlist?list=PLR4wcBxrUGPAmndrGEiN0wiaFqG-L5Yd-','https://youtube.com/playlist?list=PLawfWYMUziZqyUL5QDLVbe3j5BKWj42E5','https://youtube.com/playlist?list=PLmRNNqEA7JoN2bp1VXDZcwVgKkiI94Oha'
+    main_directory = '/home'
+    youtube_playlists_download(main_directory,youtube_playlists)
